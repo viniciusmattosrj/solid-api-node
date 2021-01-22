49b974a037b25894ead77cd185abfef6c3f23856
Vinicius Mattos <mvinicius@funcionalcorp.com.br>
2021-01-22 00:10:19 -0300

    22012021 - Adicionando o fonte do projeto.

---
+++ src/useCases/CreateUser/CreateUserUseCase.ts
@@ -0,0 +1,35 @@
+import { IUsersRepository } from "../../repositories/IUsersRepository";
+import { ICreateUserRequestDTO } from "./CreateUserDTO";
+import { User } from '../../entities/User';
+import { IMailProvider } from "../../providers/IEmailProvider";
+
+export class CreateUserUseCase {
+    constructor(
+        private usersRepository: IUsersRepository,
+        private mailProvider: IMailProvider,
+    ) {}
+
+    async execute(data: ICreateUserRequestDTO) {
+        const userAlreadyExists = await this.usersRepository.findByEmail(data.email);
+
+        if (userAlreadyExists) {
+            throw new Error('User already exists.');
+        }
+
+        const user = new User(data);
+        await this.usersRepository.save(user);
+
+        await this.mailProvider.sendMail({
+            to: {
+                name: data.name,
+                email: data.email,
+            },
+            from: {
+                name: 'Time Xablau',
+                email: 'xablau@app.com.br',
+            },
+            subject: 'Seja bem-vindo à Xablau',
+            body: '<p>Você já pode fazer login em nossa plataforma</p>'
+        });
+    }
+}
